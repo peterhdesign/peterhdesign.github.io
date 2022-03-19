a9da3b84944abd7b307b7e400b29454cefecd0db
Peter H <peterahuy@gmail.com>
2022-03-19 16:32:18 -0400

    Cleaned up code, updated images and zoom for some

---
+++ js/imagezoom.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+    $('#zoom1').zoom({
+      on: "click"
+    });
+  });
+  
+ $(document).ready(function(){
+    $('#zoom2').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom3').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom4').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom5').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom6').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom7').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom8').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom9').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom10').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom11').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom12').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom13').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom14').zoom({
+      on: "click"
+    });
+  });
+
+  $(document).ready(function(){
+    $('#zoom15').zoom({
+      on: "click"
+    });
+  });
---
+++ js/nextprevarrows.js
@@ -0,0 +1,15 @@
+$( ".previous-arrow" ).mouseenter(function() {
+    $(".previous-thumbnail img").css("opacity","1");
+  });
+  
+  $( ".previous-arrow" ).mouseout(function() {
+    $(".previous-thumbnail img").css("opacity","0");
+  });
+  
+  $( ".next-arrow" ).mouseenter(function() {
+    $(".next-thumbnail img").css("opacity","1");
+  });
+  
+  $( ".next-arrow" ).mouseout(function() {
+    $(".next-thumbnail img").css("opacity","0");
+  });
